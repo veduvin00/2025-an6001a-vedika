f9aef1e73d600d207291b824b7a6ca22964c7649
veduvin00 <83837803+veduvin00@users.noreply.github.com>
2025-11-14 03:06:47 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
+#creating application
+app = Flask(__name__)
+
+# to clear terminal : clear
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if (__name__) == "__main__":
+    app.run()
