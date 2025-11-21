7a5e121b4b911634349c12746f2267963da78515
veduvin00 <83837803+veduvin00@users.noreply.github.com>
2025-11-21 01:56:03 +0000

    1

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return(render_template("main.html"))
+
 if (__name__) == "__main__":
     app.run()
+
