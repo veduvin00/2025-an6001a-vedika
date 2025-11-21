28d74c652abd6c5aca553b4598b170359a3eef98
veduvin00 <83837803+veduvin00@users.noreply.github.com>
2025-11-21 03:10:04 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import joblib
 
 #creating application
 app = Flask(__name__)
@@ -14,6 +15,19 @@ def main():
     q = request.form.get("q")
     print(q)
     return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    print(q)
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[1.3]])
+    return(render_template("dbs_prediction.html",r=r[0][0]))
+
 
 if (__name__) == "__main__":
     app.run()
